77b674ef641b483d3fb86dc0adaadc954bb02e92
Tony Heathcoat <anthony.heathcoat@cablecast.tv>
2021-12-21 16:53:48 -0500

    video details page

--- sw-embed/src/index.js
+++ sw-embed/src/index.js
@@ -5,10 +5,10 @@ import "regenerator-runtime/runtime";
 import channelTemplate from './templates/channel.html';
 import collectionTemplate from './templates/collection.html';
 import mediaTemplate from './templates/media.html';
+import videoDetailsTemplate from './templates/video-details.html';
 import Appdata from './appdata.js';
 
 class ScreenweaveEmbed {
-
   static async setup() {
     const channelElements = document.querySelectorAll("sw-channel");
     const data = await Appdata.loadData(channelElements);
@@ -16,15 +16,17 @@ class ScreenweaveEmbed {
     for(const channelElement of channelElements) {
       ScreenweaveEmbed.normalizeChannel(channelElement);
       await ScreenweaveEmbed.insertData(channelElement, data.channelsById[data.code2id[channelElement.getAttribute("code")]]);
+      ScreenweaveEmbed.showCollections(channelElement);
     }
   }
 
   //Provide default content to empty sw-* tags and remove duplicate subcomponents
   static normalizeChannel(channelElement) {
-    if(channelElement.innerHTML.trim().length == 0)
+    if(channelElement.children.length < 1)
       channelElement.innerHTML = channelTemplate;
       ScreenweaveEmbed.normalizeFirstRemoveRest(channelElement.querySelectorAll("sw-collection"), collectionTemplate);
       ScreenweaveEmbed.normalizeFirstRemoveRest(channelElement.querySelectorAll("sw-collection sw-media"), mediaTemplate);
+      ScreenweaveEmbed.normalizeFirstRemoveRest(channelElement.querySelectorAll("sw-video-details"), videoDetailsTemplate);
   }
 
   static normalizeFirstRemoveRest(elements, template) {
@@ -69,7 +71,8 @@ class ScreenweaveEmbed {
             const mediaElementClone = oldMediaElement.cloneNode(true);
             //fill in media level data
             for(const mediaLinkElement of mediaElementClone.querySelectorAll("[sw-media-link]"))
-              mediaLinkElement.setAttribute("href", mediaObject.url);
+              mediaLinkElement.onclick = () => {ScreenweaveEmbed.showVideoDetails(channelElement, mediaObject)};
+              //mediaLinkElement.setAttribute("href", mediaObject.url);
             for(const mediaLinkElement of mediaElementClone.querySelectorAll("[sw-media-thumbnail]"))
               mediaLinkElement.setAttribute("src", mediaObject.thumbnailUrl);
             for(const mediaLinkElement of mediaElementClone.querySelectorAll("[sw-media-name]"))
@@ -87,6 +90,37 @@ class ScreenweaveEmbed {
       }
       //replace original collection tag with container of data-filled collections
       oldCollectionElement.parentNode.replaceChild(newCollectionsContainer, oldCollectionElement);
+    }
+
+    //universal parts of Video Details
+    const videoDetailsElement = channelElement.querySelector("sw-video-details");
+    if(!videoDetailsElement) {
+      console.log(`Channel ${channelData.id} contained no video details tag!`);
+    }else{
+      const backElements = videoDetailsElement.querySelectorAll("a");
+      for(const backElement of backElements)
+        backElement.onclick = () => {ScreenweaveEmbed.showCollections(channelElement)};
+    }
+  }
+
+  static showCollections(channelElement) {
+    for(const collection of channelElement.querySelectorAll("sw-collection"))
+      collection.style.display = null;
+    for(const details of channelElement.querySelectorAll("sw-video-details"))
+      details.style.display = "none";
+  }
+
+  static showVideoDetails(channelElement, mediaObject) {
+    for(const collection of channelElement.querySelectorAll("sw-collection"))
+      collection.style.display = "none";
+    for(const details of channelElement.querySelectorAll("sw-video-details")) {
+      details.style.display = null;
+      for(const thumbnailElement of details.querySelectorAll("[sw-video-details-thumbnail]"))
+        thumbnailElement.setAttribute("src", mediaObject.thumbnailUrl);
+      for(const titleElement of details.querySelectorAll("[sw-video-details-title]"))
+        titleElement.textContent = mediaObject.title;
+      for(const descriptionElement of details.querySelectorAll("[sw-video-details-description]"))
+      descriptionElement.textContent = mediaObject.description;
     }
   }
 }
